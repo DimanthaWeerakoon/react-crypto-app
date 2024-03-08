1364daa788fb0e164750db4ed47f494566d49f71
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-03-08 22:42:15 +0530

    router dom added and navbar initiated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
+import { Navbar } from "./components";
 
 const App = () => {
   return (
     <div className="app">
-      <div className="navbar"></div>
+      <div className="navbar">
+        <Navbar />
+      </div>
       <div className="main"></div>
       <div className="footer"></div>
     </div>
