11c239e16af2fe29a35c63bff2f7155001c9d820
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-03-08 21:44:35 +0530

    Dependencies Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
 
 const App = () => {
   return (
-    <div>
-      <h1>D-Alpha Cryptoverse</h1>
+    <div className="app">
+      <div className="navbar"></div>
+      <div className="main"></div>
+      <div className="footer"></div>
     </div>
   );
 };
