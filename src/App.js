2c4f706aa72a229748b92aa115f5624507727226
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-03-09 13:53:02 +0530

    Componente created and routes added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
-import { Navbar } from "./components";
+import {
+  Navbar,
+  Homepage,
+  CryptoDetails,
+  Cryptocurrencies,
+  Exchanges,
+  News,
+} from "./components";
 import "./App.css";
 
 const App = () => {
@@ -10,7 +17,23 @@ const App = () => {
       <div className="navbar">
         <Navbar />
       </div>
-      <div className="main"></div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
       <div className="footer"></div>
     </div>
   );
