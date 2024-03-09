3c5cf8cbdf78f0e1ed9342cea50e49f293c06d29
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-03-09 13:08:21 +0530

    Styles added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 import { Navbar } from "./components";
+import "./App.css";
 
 const App = () => {
   return (
