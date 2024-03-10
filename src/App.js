924110c7c917d4cb35c62a607a58b17de24d1402
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-03-10 20:03:59 +0530

    Footer created

--- src/App.js
+++ src/App.js
@@ -33,8 +33,21 @@ const App = () => {
             </Routes>
           </div>
         </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            D-Alpha Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="./">Home</Link>
+            <Link to="./exchanges">Exchanges</Link>
+            <Link to="./news">News</Link>
+          </Space>
+        </div>
       </div>
-      <div className="footer"></div>
     </div>
   );
 };
