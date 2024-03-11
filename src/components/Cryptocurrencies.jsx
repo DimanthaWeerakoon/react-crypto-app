18f6135b8cabcde6ab583d08cbb78259e7f2ba72
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-03-11 16:15:14 +0530

    Coin card limited in Homepage

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -5,16 +5,16 @@ import { Card, Col, Input, Row } from "antd";
 
 import { useGetCryptosQuery } from "../services/cryptoApi";
 
-const Cryptocurrencies = () => {
-  const { data: cryptosList, isFetching } = useGetCryptosQuery();
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 12 : 100;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
   const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
 
-  console.log(cryptos);
-
+  if (isFetching) return "Loading...";
   return (
     <>
       <Row gutter={[32, 32]} className="crpto-card-container">
-        {cryptos.map((currency) => (
+        {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
             <Link to={`/crypto/${currency.id}`}>
               <Card
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -9,7 +9,7 @@ import { Cryptocurrencies, News } from "./";
 const { Title } = Typography;
 
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(12);
   const globalStats = data?.data?.stats;
 
   if (isFetching) return "Loading...";
