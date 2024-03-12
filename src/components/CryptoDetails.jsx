605a77e6c8e31c32fac4f7a7020a59dbf2bf1f7c
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-03-12 22:39:48 +0530

    Crypot details fetched with coin id

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import HTMLReactParser from "html-react-parser";
+import { useParams } from "react-router-dom";
+import millify from "millify";
+import { Col, Row, Typography, Select } from "antd";
+import {
+  MoneyCollectOutlined,
+  DollarCircleOutlined,
+  FundOutlined,
+  ExclamationCircleOutlined,
+  StopOutlined,
+  TrophyOutlined,
+  CheckOutlined,
+  NumberOutlined,
+  ThunderboltOutlined,
+} from "@ant-design/icons";
+import { useGetCryptoDetailsQuery } from "../services/cryptoApi";
+
+const { Title, Text } = Typography;
+const { Option } = Select;
 
 const CryptoDetails = () => {
-  return <div>CryptoDetails</div>;
+  const { coinId } = useParams();
+  const [timePeriod, setTimePeriod] = useState("7d");
+  const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
+  console.log(data);
+  return <div>CryptoDetails {coinId}</div>;
 };
 
 export default CryptoDetails;
--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -30,10 +30,16 @@ const Cryptocurrencies = ({ simplified }) => {
           />
         </div>
       )}
-      <Row gutter={[32, 32]} className="crpto-card-container">
+      <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
-          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-            <Link to={`/crypto/${currency.id}`}>
+          <Col
+            xs={24}
+            sm={12}
+            lg={6}
+            className="crypto-card"
+            key={currency.uuid}
+          >
+            <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
               <Card
                 title={`${currency.rank}. ${currency.name}`}
                 extra={<img className="crypto-image" src={currency.iconUrl} />}
