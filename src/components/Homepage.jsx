ac563eee5ddda17b7b11f604276628473b13ef37
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-03-12 14:46:27 +0530

    Few changes in Homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,7 +4,7 @@ import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
 
 import { useGetCryptosQuery } from "../services/cryptoApi";
-import { useGetCryptoNewsQuery } from "../services/crptoNewsApi";
+
 import { Cryptocurrencies, News } from "./";
 
 const { Title } = Typography;
@@ -20,21 +20,30 @@ const Homepage = () => {
         {" "}
         Global Crypto Stats
       </Title>
-      <Row>
+      <Row gutter={[32, 32]}>
         <Col span={12}>
           <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
+        </Col>
+        <Col span={12}>
           <Statistic
             title="Total Exchanges"
             value={millify(globalStats.totalExchanges)}
           />
+        </Col>
+        <Col span={12}>
           <Statistic
-            title="Total Market Cap"
-            value={millify(globalStats.totalMarketCap)}
+            title="Total Market Cap:"
+            value={`$${millify(globalStats.totalMarketCap)}`}
           />
+        </Col>
+        <Col span={12}>
           <Statistic
             title="Total 24h Volume"
-            value={millify(globalStats.total24hVolume)}
+            value={`$${millify(globalStats.total24hVolume)}`}
           />
+        </Col>
+
+        <Col span={12}>
           <Statistic
             title="Total Markets"
             value={millify(globalStats.totalMarkets)}
